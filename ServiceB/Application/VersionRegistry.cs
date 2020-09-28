46892a10e6911b13df713436f8fe222f8dc077c3
user <user@localhost>
2020-09-28 23:26:15 +0200

    initial commit

---
+++ ServiceB/Application/VersionRegistry.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ServiceB.Application
+{
+    public static class VersionRegistry
+    {
+        
+        public static string Value => $"{nameof(ServiceB)} v1.0.0";
+ 
+    }
+}
